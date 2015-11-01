c8ecfa4a5ea69967f5bbcc08456dca1e75539937
23ua <artefacter@gmail.com>
2015-11-01 18:28:48 +0200

    Add Either & Maybe monads

---
+++ src/either.erl
@@ -0,0 +1,88 @@
+%%%-------------------------------------------------------------------
+%%% @author 23ua
+%%% @copyright (C) 2015
+%%% @doc
+%%% Either monad implementation
+%%%
+%%% @end
+%%%-------------------------------------------------------------------
+-module(either).
+-author("23ua").
+
+%% API
+-export([
+    either/3,
+    is_left/1,
+    is_right/1,
+    lefts/1,
+    rights/1,
+    left/1,
+    right/1,
+    map/2,
+    partition/1
+]).
+
+
+-type either() :: left() | right().
+-type left()   :: {error, any()}.
+-type right()  :: {ok, any()}.
+
+
+-spec either(fun(), fun(), either()) -> any().
+either(LeftFun, RightFun, Either) ->
+    case Either of
+        {ok, Value} ->
+            RightFun(Value);
+        {error, _} ->
+            LeftFun(Either)
+    end.
+
+
+-spec lefts([either()]) -> [either()].
+lefts(Eithers) when is_list(Eithers) ->
+    lists:filter(fun is_left/1, Eithers).
+
+
+-spec rights([either()]) -> [either()].
+rights(Eithers) when is_list(Eithers) ->
+    lists:filter(fun is_right/1, Eithers).
+
+
+-spec is_right(either()) -> boolean().
+is_right({ok, _}) ->
+    true;
+
+is_right(_) ->
+    false.
+
+
+-spec is_left(either()) -> boolean().
+is_left({error, _}) ->
+    true;
+
+is_left(_) ->
+    false.
+
+
+-spec map(fun(), either()) -> either().
+map(Fun, {ok, Val}) ->
+    right(Fun(Val));
+
+map(_, {error, _} = Left) ->
+    Left.
+
+
+-spec right(any()) -> right().
+right(Value) ->
+    {ok, Value}.
+
+
+-spec left(any()) -> left().
+left(Reason) ->
+    {error, Reason}.
+
+
+-spec partition([either()]) -> {[left()], [right()]}.
+partition([Eithers]) ->
+    lists:partition(fun is_left/1, Eithers).
+
---
+++ src/maybe.erl
@@ -0,0 +1,116 @@
+%%%-------------------------------------------------------------------
+%%% @author 23ua
+%%% @copyright (C) 2015
+%%% @doc
+%%% Maybe monad implementation
+%%%
+%%% @end
+%%%-------------------------------------------------------------------
+-module(maybe).
+-author("23ua").
+
+%% API
+-export([
+    map/2,
+    just/1,
+    nothing/0,
+    is_just/1,
+    is_nothing/1,
+    from_just/1,
+    from_maybe/2,
+    cat_maybes/1,
+    map_maybe/2,
+    maybe_to_list/1,
+    list_to_maybe/1,
+    maybe/3]).
+
+
+-type maybe()   :: just() | nothing().
+-type maybe(A)  :: just(A) | nothing().
+-type just()    :: {ok, any()}.
+-type just(A)   :: {ok, A}.
+-type nothing() :: undefined.
+
+
+-spec map(fun(), maybe()) -> maybe().
+map(Fun, {ok, Val}) ->
+    just(Fun(Val));
+
+map(_, undefined) ->
+    undefined.
+
+
+-spec maybe(Default :: any(), fun((any()) -> any()), maybe()) -> any().
+maybe(_, Fun, {ok, Val}) ->
+    Fun(Val);
+
+maybe(Default, _, undefined) ->
+    Default.
+
+
+-spec just(any()) -> just().
+just(Val) ->
+    {ok, Val}.
+
+
+-spec nothing() -> nothing().
+nothing() ->
+    undefined.
+
+
+-spec is_just(maybe()) -> boolean().
+is_just({ok, _}) ->
+    true;
+
+is_just(_) ->
+    false.
+
+
+-spec is_nothing(maybe()) -> boolean().
+is_nothing(undefined) ->
+    true;
+
+is_nothing(_) ->
+    false.
+
+
+-spec from_just(just()) -> any().
+from_just({ok, Val}) ->
+    Val.
+
+
+-spec from_maybe(A :: any(), maybe(A)) -> A.
+from_maybe(_, {ok, Val}) ->
+    Val;
+
+from_maybe(Default, undefined) ->
+    Default.
+
+
+-spec cat_maybes([maybe(A)]) -> [A].
+cat_maybes(Maybes) when is_list(Maybes) ->
+    lists:filtermap(fun ({ok, Val}) -> {true, Val}; (_) -> false end, Maybes).
+
+
+-spec map_maybe(maybe(), [any()]) -> [any()].
+map_maybe(MapFun, ListIn) ->
+    % FIXME: Maybe implement as filtermap for efficiency reasons
+    MaybeVals = lists:map(MapFun, ListIn),
+    cat_maybes(MaybeVals).
+
+
+-spec maybe_to_list(maybe()) -> list().
+maybe_to_list({ok, Val}) ->
+    [Val];
+
+maybe_to_list(_) ->
+    [].
+
+
+-spec list_to_maybe(list()) -> maybe().
+list_to_maybe([]) ->
+    nothing();
+
+list_to_maybe([Val|_]) ->
+    just(Val).
+
